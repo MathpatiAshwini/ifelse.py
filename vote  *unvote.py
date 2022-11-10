7573b79b3774ac96f9e54454cb68910a8bfcbe86
Ashwini Mathpati <110845470+MathpatiAshwini@users.noreply.github.com>
2022-11-10 09:32:57 +0530

    Add files via upload

---
+++ vote  *unvote.py
@@ -0,0 +1,10 @@
+# a=int(input("enter the age"))
+# if a>18:
+#     print("you can vote")
+# elif a<10:
+#     print("go to school")
+# elif a==50:
+#     print("take the reast")
+# else:
+#     print("no")
+
